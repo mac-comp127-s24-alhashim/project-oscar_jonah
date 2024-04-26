dfad19f6b13bc6c36ad99d590bc143b654f161ca
oscarrezab <110639308+oscarrezab@users.noreply.github.com>
2024-04-26 10:47:18 -0500

    Fix formatDescription()

--- src/RestaurantRecommendationPage.java
+++ src/RestaurantRecommendationPage.java
@@ -137,7 +137,7 @@ public class RestaurantRecommendationPage implements Page {
         for (char character: description.toCharArray()) {
             if (character == ' ') {
                 spaceCount += 1;
-                if (spaceCount%9==0) {
+                if (spaceCount%7==0) {
                     charList.add("\n");
                 }
                 else {
